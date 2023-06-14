7c114c52e587e353b797ad1bcfc8c6fbd457b0ec
Majed A <majed.atwi@gmail.com>
2023-06-14 16:57:48 -0400

    images for scrumqueu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
 });
@@ -8,3 +8,12 @@ document.addEventListener('DOMContentLoaded', function () {
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
 });
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.carousel');
+//   var instances = M.Carousel.init(elems, options);
+// });
+
+// M.AutoInit();
