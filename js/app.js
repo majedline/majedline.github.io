d5fbcb76abb9d6598d124d7a6b682f4bb8cb1842
Majed A <majed.atwi@gmail.com>
2023-06-14 15:27:51 -0400

    Modal Play on screen

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+});
